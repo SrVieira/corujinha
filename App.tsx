87768f3ea979d3bbac23a297cbd72777604c3223
SrVieira <matheusvieirasto@gmail.com>
2024-06-06 18:23:47 -0300

    change auth screen

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { StyleSheet, Text, View } from 'react-native';
+import queryClient from '@/services/queryClient';
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </QueryClientProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
